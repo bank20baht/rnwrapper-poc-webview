4f878c501a84eb307417f88eae5c8cb1429feddb
Nattapong Promthong <banker.z412@gmail.com>
2024-10-10 10:16:02 +0700

    feat: webview, getDeviceInfo, check jailbreak

--- App.tsx
+++ App.tsx
@@ -1,20 +1,92 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { useEffect, useRef, useState } from "react";
+import { SafeAreaView, Button, BackHandler, Alert } from "react-native";
+import WebView, { WebViewMessageEvent } from "react-native-webview";
+import * as Device from "expo-device";
+import JailMonkey from "jail-monkey";
 
-export default function App() {
+const App: React.FC = () => {
+  const webViewRef = useRef<WebView>(null);
+  const [deviceName, setDeviceName] = useState<string>();
+  const [modelName, setModelName] = useState<string>();
+
+  useEffect(() => {
+    if (JailMonkey.isJailBroken()) {
+      Alert.alert(
+        "Security Alert",
+        "This app cannot run on jailbreak device for security reason.",
+        [{ text: "OK", onPress: () => BackHandler.exitApp() }],
+        { cancelable: false }
+      );
+    }
+  }, []);
+  useEffect(() => {
+    const fetchDeviceInfo = async () => {
+      if (Device.isDevice) {
+        const name = await Device.deviceName;
+        setDeviceName(name || "Unknown Device");
+      } else {
+        setDeviceName("Emulator/Simulator");
+      }
+    };
+
+    const model = Device.modelName;
+    setModelName(model || "Unknown Model");
+
+    fetchDeviceInfo();
+  }, []);
+
+  const [canGoBack, setCanGoBack] = useState(false);
+
+  useEffect(() => {
+    const backAction = () => {
+      if (canGoBack) {
+        // @ts-ignore
+        webViewRef.current.goBack();
+        return true;
+      }
+      return false;
+    };
+
+    const backHandler = BackHandler.addEventListener(
+      "hardwareBackPress",
+      backAction
+    );
+
+    return () => backHandler.remove();
+  }, [canGoBack]);
+
+  const handleMessage = (event: WebViewMessageEvent) => {
+    const message = event.nativeEvent.data;
+    Alert.alert("Message from WebView", message);
+    console.log("Message from WebView", message);
+  };
+
+  const data = {
+    message: "Hello From React Native",
+    device_name: deviceName,
+    model_name: modelName,
+  };
+
+  const sendMessageToWeb = () => {
+    if (webViewRef.current) {
+      console.log("Sending message to WebView");
+      webViewRef.current.postMessage(JSON.stringify(data));
+    } else {
+      console.error("WebView reference is null.");
+    }
+  };
+
   return (
-    <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <SafeAreaView style={{ flex: 1 }}>
+      <Button title="Send Message to WebView" onPress={sendMessageToWeb} />
+      <WebView
+        ref={webViewRef}
+        source={{ uri: "https://genuine-scone-f8d469.netlify.app" }} // Replace with your web server URL
+        onMessage={handleMessage}
+        onNavigationStateChange={(navState) => setCanGoBack(navState.canGoBack)}
+      />
+    </SafeAreaView>
   );
-}
+};
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+export default App;
